8c5a06c32077f8ea1ebf16f5dcd00a4557f85591
Pao Clark <78776065+PaoComeGalletas@users.noreply.github.com>
2026-01-20 16:05:33 -0700

    First commit

---
+++ unlimited/unlimited/src/main/java/com/project/unlimited/UnlimitedApplication.java
@@ -0,0 +1,13 @@
+package com.project.unlimited;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnlimitedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnlimitedApplication.class, args);
+	}
+
+}
